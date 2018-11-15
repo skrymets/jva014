d853121b4b151f229ebd68325833a85a7e06cb8e
Sergii Krymets <skrymets@gmail.com>
2018-11-15 12:33:01 +0200

    Initial project materials commit

---
+++ src/main/java/com/luxoft/training/jva014/App.java
@@ -0,0 +1,13 @@
+package com.luxoft.training.jva014;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
