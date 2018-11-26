89f790c6ee362889416eeae87620e39a8395c575
Sergii Krymets <skrymets@gmail.com>
2018-11-26 17:32:28 +0200

    [minor] Add Application's banner

--- playground/src/main/java/com/luxoft/training/jva014/Application.java
+++ playground/src/main/java/com/luxoft/training/jva014/Application.java
@@ -15,8 +15,18 @@
  */
 package com.luxoft.training.jva014;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+//
+//     ██╗██╗   ██╗ █████╗  ██████╗  ██╗██╗  ██╗
+//     ██║██║   ██║██╔══██╗██╔═████╗███║██║  ██║
+//     ██║██║   ██║███████║██║██╔██║╚██║███████║
+//██   ██║╚██╗ ██╔╝██╔══██║████╔╝██║ ██║╚════██║
+//╚█████╔╝ ╚████╔╝ ██║  ██║╚██████╔╝ ██║     ██║
+// ╚════╝   ╚═══╝  ╚═╝  ╚═╝ ╚═════╝  ╚═╝     ╚═╝
 
 /**
  *
@@ -24,6 +34,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  */
 @SpringBootApplication
 public class Application {
+    
+    private static final Logger LOG = LoggerFactory.getLogger(Application.class);
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
